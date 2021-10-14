8d6a57108c47abe8115f05f57b91c75af8692651
ds-astroboy <davidsanter397@gmail.com>
2021-10-14 10:24:47 +0600

    png file added

---
+++ views/default.py
@@ -0,0 +1,16 @@
+
+# Dash packages
+import dash_bootstrap_components as dbc
+from dash import Dash, callback, html, dcc, dash_table, Input, Output, State, MATCH, ALL
+# import dash_html_components as html
+
+from app import app
+
+
+###############################################################################
+########### LANDING PAGE LAYOUT ###########
+###############################################################################
+layout = dbc.Container([
+        html.H2('Welcome to Dashboard'),
+        html.Hr(),
+], className="mt-4")
